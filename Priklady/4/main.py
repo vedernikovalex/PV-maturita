823176be4ebe168e0fb6d49facc9f70b80122290
Alexander Vedernikov <alexander.vedernikov@viableone.com>
2023-05-04 00:08:50 +0200

    4 and 5

---
+++ Priklady/4/main.py
@@ -0,0 +1,26 @@
+#lambdas
+cislo = 12
+pridej_sto = lambda x : x + 100 if isinstance(x, (int, float)) else None
+print(pridej_sto(cislo))
+
+#delegat
+def pricti_cisla(x, y):
+    return x + y
+
+def odecti_cisla(x, y):
+    return x - y
+
+def vypocet(operace, x, y):
+    return operace(x, y)
+
+pricitat_delegat = pricti_cisla
+odecitat_delegat = odecti_cisla
+
+result = vypocet(pricitat_delegat, 5, 3)
+print("Přičítání "+str(result))
+
+result = vypocet(odecitat_delegat, 5, 3)
+print("Odečítání "+str(result))
+
+
+
