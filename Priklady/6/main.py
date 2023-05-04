e48505011aff290e1d11d82e012f162b8d7628ab
Alexander Vedernikov <alexander.vedernikov@viableone.com>
2023-05-04 15:17:51 +0200

    6 7

---
+++ Priklady/6/main.py
@@ -0,0 +1,50 @@
+import math
+import sys
+import time
+
+def my_function(n):
+    result = []
+    for i in range(n):
+        result.append(i)
+
+
+input_sizes_time = [10000, 100000, 1000000, 10000000, 100000000]
+
+def measure_time_complexity():
+    for n in input_sizes_time:
+        start = time.time()
+        my_function(n)
+        end = time.time()
+        print("for "+str(n))
+        result = end - start
+        print("this time: "+str(result))
+
+
+def data_complexity_n(n):
+    # create a list of size n
+    data = [0] * n
+    # return the size of the list
+    return len(data)
+
+
+def data_complexity_logn(n):
+    # calculate the log base 2 of n
+    log_n = math.log2(n)
+    # return the calculated value
+    return log_n
+
+
+# Example usage:
+input_sizes_data = [10, 100, 1000, 10000, 1000000]
+
+def measure_data_complexity():
+    for n in input_sizes_data:
+        complexity_n = data_complexity_n(n)
+        complexity_log = data_complexity_logn(n)
+        print(f"The N data complexity of {n} is O({complexity_n}).")
+        print(f"The LOG N data complexity of {n} is O({complexity_log}).")
+        print("")
+
+
+measure_time_complexity()
+measure_data_complexity()
---
+++ Priklady/hovinko/main.py
@@ -0,0 +1,5 @@
+import logging
+
+logging.basicConfig(filename="log.txt")
+logging.error("nastala velika chyba")
+
