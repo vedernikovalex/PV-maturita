2017784b79a8306a53e1ce04ae0ed38bd4d2752c
Alexander Vedernikov <alexander.vedernikov@viableone.com>
2023-05-14 02:41:34 +0200

    prubezne opakovani erektilni disfunkce muze zpusobovat psychicky stress

---
+++ Priklady/VS_22/VS_22/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Prgram
+{
+    static int cislo = 0;
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("PUVDNI: " + cislo);
+        Thread t1 = new Thread(()=>
+        {
+            int temp = cislo;
+            temp++;
+            Thread.Sleep(1000);
+            cislo = temp;
+            Console.WriteLine("Pricetlo, cislo ted: " + cislo);
+        });
+        Thread t2 = new Thread(() =>
+        {
+            int temp = cislo;
+            temp--;
+            Thread.Sleep(1000);
+            cislo = temp;
+            Console.WriteLine("Odecetlo, cislo ted: " + cislo);
+        });
+
+        t1.Start();
+        t2.Start();
+
+        t1.Join();
+        t2.Join();
+
+        Console.WriteLine("AHA: " + cislo);
+
+    }
+
+}
